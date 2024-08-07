8a397b688be874ab0b4b40651f1cc5f03d6ec839
Serifeeng <serifeenginarrr@gmail.com>
2024-08-07 22:03:58 +0300

    Book-Store

--- store/src/App.js
+++ store/src/App.js
@@ -1,31 +1,3 @@
-// import React, { useState } from 'react';
-// import './css/App.css';
-// import BookTable from './BookTable';
-// import BookImages from './BookImages';
-
-// function App() {
-//   const [theme, setTheme] = useState('dark');
-
-//   const toggleTheme = () => {
-//     setTheme(theme === 'dark' ? 'light' : 'dark');
-//   };
-
-//   return (
-//     <div className={`App ${theme}`}>
-    
-//       <div className='title'><h1>BOOK STORE</h1></div>
-//       <div>
-//       <button className='toggle-button' onClick={toggleTheme}>Dark Mod On</button>
-//       </div>
-
-//       <BookImages />
-//       <BookTable />
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import React, { useEffect,useState} from 'react';
 import './css/App.css';
 import BookTable from './BookImages';
